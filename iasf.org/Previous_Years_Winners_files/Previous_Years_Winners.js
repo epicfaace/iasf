d3dd81f7d9231d3166b0196bedf54309a19b0230
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-07-16 16:15:53 -0400

    iasf initial

---
+++ iasf.org/Previous_Years_Winners_files/Previous_Years_Winners.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20110912
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Previous_Years_Winners_files/Previous_Years_WinnersMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ iasf/content/post/test.md
@@ -0,0 +1,8 @@
++++
+date = "2017-07-16T15:56:13-04:00"
+draft = true
+title = "test"
+
++++
+
+This is a **great** test post. AHU!!!
