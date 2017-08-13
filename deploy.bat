d79bb00c844efc6a99463200a2ecd4a78adb3cd6
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-08-13 12:52:06 -0400

    remove public from master, config to use https urls

--- deploy.bat
+++ deploy.bat
@@ -1,26 +1,6 @@
 rmdir /s /q public
 mkdir public
-
-
-
-REM clone gh-pages branch from the local repo into a repo located within "public"
-
 git clone .git --branch gh-pages public
-
-  
-
-REM generate
-
 hugo
-
-  
-
-REM commit the changes in the clone and push them back to the local gh-pages branch    
-
 cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
-
-
-
-REM publish
-
 git push origin gh-pages
