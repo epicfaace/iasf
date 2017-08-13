aebb67eac349bc7d76c8f43a82c17807cae3b8b3
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-08-13 13:04:23 -0400

    deploy bat fixes

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,6 @@ rmdir /s /q public
 mkdir public
 git clone .git --branch gh-pages public
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+cd public && git add --all && git remote set-url origin https://github.com/epicfaace/iasf.git && git commit -m "Publishing to gh-pages" && git push origin gh-pages
 git push origin gh-pages
+cd ..
