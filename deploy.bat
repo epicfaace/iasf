aa0512d1260380e726d4899f738082829aca6e25
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-08-13 12:02:03 -0400

    Publishing to gh-pages

---
+++ deploy.bat
@@ -0,0 +1,26 @@
+rmdir public
+mkdir public
+
+
+
+# clone gh-pages branch from the local repo into a repo located within "public"
+
+git clone .git --branch gh-pages public
+
+  
+
+# generate
+
+hugo
+
+  
+
+# commit the changes in the clone and push them back to the local gh-pages branch    
+
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+
+
+
+# publish
+
+git push upstream gh-pagesk
