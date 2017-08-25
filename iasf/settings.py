17a72247156381b740f6bdca12032d07c6e62a50
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-08-25 08:55:12 -0400

    more stuff

--- iasf/settings.py
+++ iasf/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apply.apps.ApplyConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
