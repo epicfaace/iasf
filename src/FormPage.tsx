8950968e36600b90870e575cb86574456458e8ea
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-10-20 14:09:47 -0700

    first page

--- src/FormPage.tsx
+++ src/FormPage.tsx
@@ -46,105 +46,74 @@ const schema = {
         "country":          { "type": "string" }
       },
       "required": ["street_address", "city", "state"]
+    },
+    "name": {
+      type: 'object',
+      title: '',
+      properties: {
+        'first': {type: 'string', title: 'First Name'},
+        'middle': {type: 'string', title: 'Middle Name'},
+        'last': {type: 'string', title: 'Last Name'}
+      }
     }
   },
-  title: 'Balavihar Registration',
+  title: 'Registration',
   type: 'object',
   required: ['title'],
   properties: {
-    members: {
-      title: 'Members',
-      type: 'array',
-      items: {
+      name: { "$ref": "#/definitions/name" },
+      email: {type: 'string', format: 'email'},
+      phone: {
         type: 'object',
+        title: '',
         properties: {
-          name: {
-            type: 'object',
-            title: '',
-            properties: {
-              'first': {type: 'string', title: 'First Name'},
-              'middle': {type: 'string', title: 'Middle Name'},
-              'last': {type: 'string', title: 'Last Name'}
-            }
-          },
-          phone: {
-            type: 'object',
-            title: '',
-            properties: {
-              'home': {type: 'string', title: 'Home Phone'},
-              'cell': {type: 'string', title: 'Cell Phone'},
-              'work': {type: 'string', title: 'Office Phone'}
-            }
-          },
-          email: {type: 'string', format: 'email'},
-          profession: {
-            type: "string",
-            title: "Profession",
-            enum: ["Account Manager","Alternative Medicine","Architecture","Art & Fabrics","Biostatistician",
-                  "Clinical Lab Scientist","Clinical Research","Dance & Fine Arts","Dentist",
-                  "Engineering - Biomedical","Engineering - Chemical","Engineering - Civil",
-                  "Engineering - Electrical","Engineering - Electronics", "Engineering - Environmental",
-                  "Engineering - Industrial","Engineering - Management","Engineering - Mechanical",
-                  "Engineering - Metallurgy","Engineering - Other","Entreprenuer - Construction",
-                  "Entreprenuer - Education","Entreprenuer - Electronics","Entreprenuer - Food Industry",
-                  "Entreprenuer - Hospitality Industry","Entreprenuer - Import & Export","Entreprenuer - Other",
-                  "Entreprenuer - Real Estate","Entreprenuer - Software Industry","Finance - Accounting",
-                  "Finance - CPA","Finance - Mortgage","Finance - Other","Finance - Personal Banker",
-                  "Finance - Personal Finance","Finance - Tax Consulting","Finance - Wealth Management",
-                  "Front Office Management","Government","Graduate Student","Histotechnologist","Homemaker",
-                  "Hotel Management","Human Resources","Insurance Agents","Journalism","Lab Associate","Lawyer",
-                  "Medical Assistant","Network Engineer","Nurse","Pharmacist","Physician - Cardiology",
-                  "Physician - Internal Medicine","Physician - Nephrologist","Physician - Neurosurgeon",
-                  "Physician - OB/GYN","Physician - Oncology","Physician - Ophthalmologists",
-                  "Physician - Optometrist","Physician - Orthopedics","Physician - Other","Physician - Pediatrician",
-                  "Professor - College","Project Management","Psychotherapist","Regulatory Affairs","Rehabilitation",
-                  "Research","Sales & Marketing","Social Work","Software - Big Data/Statistics","Software - Business/System Analyst",
-                  "Software - Cloud (AWS)","Software - Cloud (others)","Software - Database","Software - Development",
-                  "Software - iOS/Android Development","Software - Management/Executive","Software - Other","Software - Testing",
-                  "Software - UI/UX Designer","Teacher - Elementary School","Teacher - High School","Teacher - Middle School",
-                  "Teacher - Other","Teacher - PreSchool","Teacher - Private Tutoring"]
-            // enumNames: ["one", "two", "three"]
-          },
-          address: { "$ref": "#/definitions/address" }
+          'home': {type: 'string', title: 'Home Phone'},
+          'cell': {type: 'string', title: 'Cell Phone'}
         }
-      }
-    } // members
-  }
+      },
+      descent: {
+        type: "string",
+        title: "Claim to Indian Descent",
+        enum: ["Maternal grandparents", "Paternal grandparents"],
+        enumNames: ["Maternal grandparents", "Paternal grandparents"]
+      },
+      where: {
+        type: "string",
+        title: "Where did you hear about IASF from?",
+        enum: ["Facebook", "Internet search", "High school", "Friends", "Family", "Other"],
+        enumNames: ["Facebook", "Internet search", "High school", "Friends", "Family", "Other"]
+      },
+      address: { "$ref": "#/definitions/address" },
+      parentName: { "$ref": "#/definitions/name" }
+    }
 };
 
 
 const uiSchema =  {
-    title: {
-      "ui:widget": "color" // could also be "select"
-    },
-    members: {
-      "ui:options":  {
-        orderable: false
+  classNames: 'mainForm container-fluid',
+    name: {
+      classNames: 'threeColumn',
+      /*first: {
+        classNames: 'col-xs-12 col-sm-4'
       },
-      items: {
-        name: {
-          classNames: 'threeColumn',
-          first: {
-            "ui:placeholder": "First Name",
-            'ui-widget': 'phoneWidget'
-          }
-        },
-        phone: {
-          classNames: 'threeColumn',
-          home: {
-            'ui-placeholder': 'home phone'
-          }
-        },
-        email: {
-          classNames: 'halfWidth'
-        },
-        profession: {
-          classNames: 'halfWidth'
-        },
-        address: {
-          classNames: 'threeColumn'
-        }
-      } // members
+      middle: {
+        classNames: 'col-xs-12 col-sm-4'
+      },
+      last: {
+        classNames: 'col-xs-12 col-sm-4'
+      },*/
+    },
+    parentName: {
+      classNames: 'threeColumn'
+    },
+    address: {
+      classNames: 'flexColumn',
+      street_address: {
+        classNames: 'col-padding-0 col-xs-12'
+      }
+    },
+    descent: {
+      classNames: 'col-xs-12 col-sm-6'
     }
 
   };
@@ -160,7 +129,7 @@ class FormPage extends React.Component {
   render() {
     return (
         <div className="App">
-            <h1>Chinmaya Mission Worldwide</h1>
+            <h1>IASF application</h1>
             <Form
               schema={schema}
               uiSchema={uiSchema}
