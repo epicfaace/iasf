d592e7c72454816e4444e30b7c016f1f81137207
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-10-25 15:00:28 -0700

    some more fields and pages

--- iasf/apply/models.py
+++ iasf/apply/models.py
@@ -57,6 +57,30 @@ Any additional documents (optional)
                     "college_name","college_received_acceptance_letter")
                 })
             )
+        },
+        {
+            "name": "Essay",
+            "fields": (
+                ("essay")
+            )
+        },
+        {
+            "name": "Activities",
+            "fields": (
+                ("")
+            )
+        },
+        {
+            "name": "Financial Information",
+            "fields": (
+                ("")
+            )
+        },
+        {
+            "name": "Submit",
+            "fields": (
+                ("essay")
+            )
         }
     ]
     CLAIM_INDIAN_DESCENT_CHOICES = (
@@ -72,12 +96,12 @@ Any additional documents (optional)
     email = models.EmailField(blank=True)
     phone_home = models.CharField(validators=[phone_regex], max_length=15, blank=True, verbose_name="Home Phone") # validators should be a list
     phone_mobile = models.CharField(validators=[phone_regex], max_length=15, blank=True, verbose_name="Mobile Phone") # validators should be a list
-    claim_indian_descent = models.IntegerField(null=True, choices=CLAIM_INDIAN_DESCENT_CHOICES, verbose_name="Claim to Indian descent")
+    claim_indian_descent = models.IntegerField(null=True, blank=True, choices=CLAIM_INDIAN_DESCENT_CHOICES, verbose_name="Claim to Indian descent")
     
     home_address_1 = models.CharField(_("address"), max_length=128, blank=True)
     home_address_2 = models.CharField(_("address cont'd"), max_length=128, blank=True)
-    home_city = models.CharField(_("city"), max_length=64, default="")
-    home_state = models.CharField(_("state"), max_length=2, default="GA")
+    home_city = models.CharField(_("city"), max_length=64, default="", blank=True)
+    home_state = models.CharField(_("state"), max_length=2, default="GA", blank=True)
     home_zip_code = models.CharField(_("Zip Code"), max_length=5, default="")
 
     parent_first_name = models.CharField(max_length=50, blank=True)
@@ -85,12 +109,12 @@ Any additional documents (optional)
     parent_last_name = models.CharField(max_length=50, blank=True)
 
     # second page:
-    hs_name = models.CharField(max_length=100)
+    hs_name = models.CharField(max_length=100, blank=True)
     
     hs_address_1 = models.CharField(_("Address Line 1"), max_length=128, blank=True)
     hs_address_2 = models.CharField(_("Address Line 2"), max_length=128, blank=True)
-    hs_city = models.CharField(_("City"), max_length=64, default="")
-    hs_state = models.CharField(_("State"), max_length=2, default="GA")
+    hs_city = models.CharField(_("City"), max_length=64, default="", blank=True)
+    hs_state = models.CharField(_("State"), max_length=2, default="GA", blank=True)
     hs_zip_code = models.CharField(_("Zip Code"), max_length=5, default="")
     
     hs_counselor_first_name = models.CharField(max_length=50, blank=True)
@@ -99,8 +123,8 @@ Any additional documents (optional)
     hs_counselor_email = models.EmailField(blank=True)
 
     hs_gpa = models.DecimalField(blank=True, null=True, max_digits=3, decimal_places = 1)
-    hs_class_rank = models.IntegerField(null=True)
-    hs_class_size = models.IntegerField(null=True)
+    hs_class_rank = models.IntegerField(null=True, blank=True)
+    hs_class_size = models.IntegerField(null=True, blank=True)
 
     scores_sat_reading = models.IntegerField(blank=True, null=True)
     scores_sat_math = models.IntegerField(blank=True, null=True)
@@ -120,8 +144,21 @@ Any additional documents (optional)
     college_name = models.CharField(_("College name"), blank=True, max_length=100)
     college_received_acceptance_letter = models.NullBooleanField(_("I have received an acceptance letter."), max_length=100)
 
-    #date_created = models.Date
-    def __str__(self):              # __unicode__ on Python 2
+    # PAGE 3: ESSAY
+    essay = models.TextField(blank=True, null=True)
+
+    # PAGE 4: FINANCIAL INFORMATION
+    #income_parent = 
+    #income_student = models.
+    list_dependents = models.TextField(_("List of dependents currently entering college"), blank=True, null=True) # todo: jsonfield.
+    
+    form = []
+
+    date_created = models.DateField(null=True, blank=True)
+    date_last_submitted = models.DateField(null=True, blank=True)
+    year = models.IntegerField(null=False, blank=False, default="2018")
+
+    def __str__(self):
         return self.first_name
     @classmethod
     def getFields(self, number):
--- iasf/apply/static/apply/css/form.js
+++ iasf/apply/static/apply/css/form.js
@@ -1,7 +1,10 @@
 $(function() {
+    // when button save clicked, submit the form.
     $(".btnSave").click(function() {
         $(this).closest("form").submit();
     });
+
+    // when page link clicked, first submit the current form.
     $("a.pageLink").click(function(e) {
         e.preventDefault();
         var url = $(this).attr("href");
@@ -9,7 +12,7 @@ $(function() {
         $.post("", $form.serialize()).success(function() {
             window.location.href = url;
         }).fail(function() {
-            window.location.href = url;
+            alert("There was an error submitting the form. Please fix the errors and try again.");
         });
     });
 });
