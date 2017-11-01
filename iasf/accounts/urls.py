35690c3eaa2820a758d0db134969161b1e726afc
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-10-31 20:52:05 -0700

    login and signup views init

--- iasf/accounts/urls.py
+++ iasf/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from .views import SignupView
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url('^signup', SignupView.as_view(), name='signup' )
 ]
--- iasf/accounts/views.py
+++ iasf/accounts/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
 # Create your views here.
+class SignupView(FormView):
+    template_name = 'registration/signup.html'
+    # form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(SignupView, self).form_valid(form)
