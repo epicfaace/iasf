73f9e1801a813d7f0542565f49c85f8c4c5e5d09
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-11-02 09:52:32 -0700

    attached applications to admin

--- iasf/apply/admin.py
+++ iasf/apply/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Application, ApplicationAdmin)
